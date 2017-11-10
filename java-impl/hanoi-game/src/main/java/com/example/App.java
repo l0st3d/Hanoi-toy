548122f725fcfae1cf02320aeced82e2e2e2de25
Ed Bowler <ed.bowler@gmail.com>
2017-11-10 19:06:45 +0000

    added demo

--- java-impl/hanoi-game/src/main/java/com/example/App.java
+++ java-impl/hanoi-game/src/main/java/com/example/App.java
@@ -5,6 +5,8 @@ import com.example.model.*;
 public class App {
     public static void main( String[] args ) {
         Game game = Game.create(5);
-        
+        System.out.println(game);
+        Solver.solve(game);
+        System.out.println(game);
     }
 }
